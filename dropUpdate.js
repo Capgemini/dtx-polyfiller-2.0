225b8f5dce8739c56c0e8e5fddc9c973bc85e95a
FreddieDev <freddiechessell83@gmail.com>
2019-11-28 10:11:33 +0000

    Refactor and cleanup of git to no longer use Yarn.

--- dropUpdate.js
+++
@@ -1,7 +0,0 @@
-const fs = require("fs");
-
-let manifest = JSON.parse(fs.readFileSync("./DTX/manifest.json"));
-
-delete manifest.update_url;
-
-fs.writeFileSync("./DTX/manifest.json", JSON.stringify(manifest));
